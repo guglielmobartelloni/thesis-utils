2970a40c13903525eccd52b387db5b57fd2706b4
samoorai <hirotakatech@gmail.com>
2023-03-27 14:52:24 +0200

    Added xgboost test

--- soft_brownian_offset_plotting.py
+++ soft_brownian_offset_plotting.py
@@ -5,6 +5,9 @@
 import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
 
 from umap import UMAP
 
@@ -17,6 +20,9 @@ import itertools
 import numpy as np
 from sbo import soft_brownian_offset
 
+model = xgb.XGBClassifier()
+
+
 input_data = pd.read_csv('datasets/ADFANet_Shuffled_LabelOK.csv')
 
 # n_normal_samples = 500
@@ -24,7 +30,7 @@ input_data = pd.read_csv('datasets/ADFANet_Shuffled_LabelOK.csv')
 
 # the number of normal samples are 1.5% of the initial data (for performance)
 # n_normal_samples = int(len(csv_data) * .015)
-n_normal_samples = int(len(input_data) * .035)
+n_normal_samples = int(len(input_data) * .015)
 
 # the number ood samples are 110% of the initial data
 # n_ood_samples = n_normal_samples + int(n_normal_samples * .1)
@@ -67,6 +73,8 @@ for (i, (d_min_, softness_)) in enumerate(itertools.product(d_min, softness)):
 
     # tmp variable to store the data
     data = data_initial
+    X_without_ood = data
+    y_without_ood = data_used.label
 
     # Run the soft brownian offset algorithm
     data_ood = soft_brownian_offset(data, d_min_, d_off_,
@@ -77,26 +85,68 @@ for (i, (d_min_, softness_)) in enumerate(itertools.product(d_min, softness)):
     data = np.concatenate((data, data_ood, input_data[input_data['label'] == "normal"].drop(
         columns=['label']).to_numpy()))
 
+    # For comprension
+    X_with_ood = data
+
     # Merge the initial labels with the OOD labels
     labels = np.concatenate((data_used.label, [
         'ood' for x in range(n_ood_samples)], input_data[input_data['label'] == "normal"].label))
 
-    umap_2d = UMAP(n_components=2, init='random', random_state=0,
-                   n_neighbors=70, min_dist=.8)
-    proj_2d = umap_2d.fit_transform(data)
+    labels_for_model = np.concatenate((['attack' for x in range(len(data_used))], [
+        'attack' for x in range(n_ood_samples)], input_data[input_data['label'] == "normal"].label))
 
-    fig.add_trace(go.Scatter(
-        x=proj_2d[:, 0],
-        y=proj_2d[:, 1],
-        mode='markers',
-        marker=dict(
-            color=transform_color(labels),
-            colorscale='Viridis',
-            showscale=True
-        )), row=row, col=col)
+    # Normalize the labels for the model
+    le = LabelEncoder()
+    le.fit(labels_for_model)
+    y_with_ood = le.transform(labels_for_model)
 
-    # fig_2d = px.scatter(proj_2d, x=0, y=1, color=labels,
-    #                     labels={'color': 'label'})
-    # fig_2d.show()
+    # Normalize the labels for the model
+    le = LabelEncoder()
+    le.fit(data_used.label)
+    y_without_ood = le.transform(data_used.label)
 
-fig.show()
+    # Separate the data
+    X_with_ood_train, X_with_ood_test, y_with_ood_train, y_with_ood_test = train_test_split(
+        X_with_ood, y_with_ood, test_size=0.5, random_state=123)
+
+    X_without_ood_train, X_without_ood_test, y_without_ood_train, y_without_ood_test = train_test_split(
+        X_without_ood, y_without_ood, test_size=0.5, random_state=123)
+
+    model = xgb.XGBClassifier()
+    print(len(X_with_ood_train))
+    print(len(y_with_ood_train))
+    # Train the model
+    model.fit(X_with_ood_train, y_with_ood_train)
+
+    y_with_ood_pred = model.predict(X_without_ood_test)
+    accuracy_with_ood = accuracy_score(y_without_ood_test, y_with_ood_pred)
+    metthews_with_ood = matthews_corrcoef(y_without_ood_test, y_with_ood_pred)
+
+    model.fit(X_without_ood_train, y_without_ood_train)
+
+    y_without_ood_pred = model.predict(X_without_ood_test)
+    accuracy_without_ood = accuracy_score(
+        y_without_ood_test, y_without_ood_pred)
+    metthews_without_ood = matthews_corrcoef(
+        y_without_ood_test, y_without_ood_pred)
+
+    print("WITH OOD Accuracy: {:.2f}\tMetthews: {:.2f}".format(
+        accuracy_with_ood, metthews_with_ood))
+    print("WITHOUT OOD Accuracy: {:.2f}\tMetthews: {:.2f}".format(
+        accuracy_without_ood, metthews_without_ood))
+
+    # umap_2d = UMAP(n_components=2, init='random', random_state=0,
+    #                n_neighbors=70, min_dist=.8)
+    # proj_2d = umap_2d.fit_transform(data)
+    #
+    # fig.add_trace(go.Scatter(
+    #     x=proj_2d[:, 0],
+    #     y=proj_2d[:, 1],
+    #     mode='markers',
+    #     marker=dict(
+    #         color=transform_color(labels),
+    #         colorscale='Viridis',
+    #         showscale=True
+    #     )), row=row, col=col)
+
+# fig.show()
