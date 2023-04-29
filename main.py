7fc77349dbe06bd5ad88bfdac527d2f4e9ae199c
samoorai <hirotakatech@gmail.com>
2023-04-29 15:03:43 +0200

    Improved code a lot scomposition

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 
-from utils import preprocess_adfa, preprocess_cicids
+from utils import preprocess_adfa, preprocess_cicids, generate_ood, merge_data
+import pandas as pd
+import numpy as np
 
 
+# N+(N+AV OOD) 
+# N+AV+(N + AV OOD)
+# N+N OOD 
+# N+AV+N OOD (FATTO)
+# N+AV OOD 
+# N+AV+AV OOD (FATTO)
+
+N = 1000
+N_ood = 1000
+
+cicids_dataset = pd.read_csv('./datasets/CICIDS18_Shuffled_Reduced.csv')
+
+normal_samples, attack_samples = preprocess_cicids(cicids_dataset, N)
+
+ood_samples = generate_ood(attack_samples, N_ood, 0.25, 0.0)
+
+merged_samples, labels= merge_data(normal_samples, attack_samples, ood_samples)
+
+# Check if the number of samples are right
+assert len(merged_samples) == N + N_ood and len(labels) == N + N_ood
 
 
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,15 @@
 import numpy as np
 from sbo import soft_brownian_offset
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+
 
 def preprocess_cicids(input_data, n_samples):
 
     # reduce the number of sample data
     input_data = input_data[0:n_samples]
+    input_data.drop(columns=['Timestamp'], inplace=True)
     input_data.replace([np.inf, -np.inf], -1, inplace=True)
     input_data.replace(np.nan, -1, inplace=True)
     # Select only attack samples
@@ -40,10 +45,40 @@ def preprocess_adfa(input_data, n_normal_samples):
 
     return (normal_packets, attacks_packets)
 
-def generate_ood(starting_data, n_ood_samples, d_min,softness):
+
+def generate_ood(starting_data, n_ood_samples, d_min, softness):
     d_off = d_min * .7
-    return soft_brownian_offset(starting_data, d_min, d_off,
+    return soft_brownian_offset(starting_data.drop(columns=['label']).to_numpy(), d_min, d_off,
                                 softness=softness,
                                 n_samples=n_ood_samples)
 
 
+# Merge the initial data with the OOD data and normal data
+def merge_data(normal_data, attacks_data, ood_data):
+    return (np.concatenate((normal_data.drop(columns=['label']), attacks_data.drop(columns=['label']), ood_data)), merge_labels(normal_data, attacks_data, ood_data))
+
+# Merge the initial labels with the OOD labels
+
+
+def merge_labels(normal_data, attacks_data, ood_data):
+    return np.concatenate((normal_data.label, attacks_data.label, ['attack' for x in range(len(ood_data))]))
+
+
+def train_and_save_model(data,labels, model_path):
+    # Normalize the labels for the model
+    encoder = LabelEncoder()
+    y = encoder.fit_transform(labels.reshape(-1, 1))
+    X = data_i
+
+    # Separate the data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=123)
+
+    model = xgb.XGBClassifier()
+    # Train the model
+    model.fit(X_train, y_train)
+
+    y_predicted = model.predict(X_test)
+
+    # Save the model
+    model.save_model(model_path)
